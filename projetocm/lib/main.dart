2a1362e02fd1ec2487d3c982c4bceb8280f34659
AfonsoCoelho <202100258@estudantes.ips.pt>
2024-06-10 16:21:35 +0100

    pages

--- projetocm/lib/main.dart
+++ projetocm/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tony Chopper',
+      title: 'Tony Tony Chopper',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,24 +23,56 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tony Tony Chopper'),
-        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
         actions: [
           IconButton(
-            icon: Icon(Icons.home), // Ícone de exemplo, pode ser substituído
+            icon: Icon(Icons.store), // Ícone da loja
             onPressed: () {
-              // Ação vazia
+              // Navegar para a página da loja
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LojaPage()),
+              );
             },
           ),
           IconButton(
-            icon: Icon(Icons.search), // Ícone de exemplo, pode ser substituído
+            icon: Icon(Icons.inventory), // Ícone do inventário
             onPressed: () {
-              // Ação vazia
+              // Navegar para a página do inventário
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => InventarioPage()),
+              );
             },
           ),
           IconButton(
-            icon: Icon(Icons.settings), // Ícone de exemplo, pode ser substituído
+            icon: Icon(Icons.person), // Ícone do perfil
             onPressed: () {
-              // Ação vazia
+              // Navegar para a página do perfil
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PerfilPage()),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.help_outline), // Ícone vazio (placeholder)
+            onPressed: () {
+              // Navegar para a página do jogo
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GamePage()),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz), // Ícone vazio (placeholder)
+            onPressed: () {
+              // Navegar para a página do transformacao
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TransformationPage()),
+              );
             },
           ),
         ],
@@ -54,13 +86,94 @@ class HomePage extends StatelessWidget {
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 50.0,
-          color: Colors.blue,
+          color: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para o Footer
           alignment: Alignment.center,
           child: Text(
-            'Tony Chopper',
+            'Tony Tony Chopper',
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
         ),
+      ),
+    );
+  }
+}
+
+// Página da Loja
+class LojaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Loja'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
+      ),
+      body: Center(
+        child: Text('Página da Loja', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+// Página do Inventário
+class InventarioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inventário'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
+      ),
+      body: Center(
+        child: Text('Página do Inventário', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+// Página do Perfil
+class PerfilPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
+      ),
+      body: Center(
+        child: Text('Página do Perfil', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+
+// Página do mini-jogo
+class GamePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mini-Jogo'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
+      ),
+      body: Center(
+        child: Text('Página do Mini-Jogo', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+// Página do Perfil
+class TransformationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transformações'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê para a AppBar
+      ),
+      body: Center(
+        child: Text('Página das Transformações', style: TextStyle(fontSize: 24)),
       ),
     );
   }
