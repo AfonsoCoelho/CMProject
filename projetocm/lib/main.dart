2ef306d0e4c62386e306f81a45a222a4e66bf318
AfonsoCoelho <202100258@estudantes.ips.pt>
2024-06-10 13:07:44 +0100

    splash

--- projetocm/lib/main.dart
+++ projetocm/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tony Tony Chopper',
+      title: 'Tony Chopper',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,11 +23,32 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tony Tony Chopper'),
+        backgroundColor: Color(0xFFFFC0CB), // Cor de fundo rosa bebê
+        actions: [
+          IconButton(
+            icon: Icon(Icons.home), // Ícone de exemplo, pode ser substituído
+            onPressed: () {
+              // Ação vazia
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search), // Ícone de exemplo, pode ser substituído
+            onPressed: () {
+              // Ação vazia
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.settings), // Ícone de exemplo, pode ser substituído
+            onPressed: () {
+              // Ação vazia
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: Text(
-          'Página Inicial',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: Image.asset(
+          'assets/splash.png', // Caminho da imagem
+          fit: BoxFit.cover, // Ajusta a imagem ao tamanho do widget
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -36,7 +57,7 @@ class HomePage extends StatelessWidget {
           color: Colors.blue,
           alignment: Alignment.center,
           child: Text(
-            'Tony Tony Chopper',
+            'Tony Chopper',
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
         ),
